776f53627ae8c7ddda322a93004102c2aab5ade1
Federico Sananes <federico.sananes@gmail.com>
2017-07-17 04:50:02 -0300

    precond heu ok

--- my_air_cargo_problems.py
+++ my_air_cargo_problems.py
@@ -212,6 +212,17 @@ class AirCargoProblem(Problem):
         """
         # TODO implement (see Russell-Norvig Ed-3 10.2.3  or Russell-Norvig Ed-2 11.2)
         count = 0
+        state = node.state
+        print(state)
+        goal_literals = self.goal 
+        print(goal_literals)
+        fl = decode_state(state, self.state_map)
+        print(fl)
+        for g in goal_literals: 
+            if g not in fl.pos:
+                count +=1
+
+
         return count
 
 
